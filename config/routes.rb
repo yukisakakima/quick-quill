58622ecb4180bb1d28389c6c9f8f2fa302a038ce
yukisakakima <kima87161211@gmail.com>
2024-05-26 18:38:44 +0900

    initialize後コミット

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
+  root 'home#top'
+  get "about" => "home#about"
+  resources :users, only: [:show, :edit, :update]
+  post "users/:id/update" => "users#update"
+
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+end
